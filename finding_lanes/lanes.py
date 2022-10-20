158a0f4f1ffd8dba2d251b48468c79b9020e5433
QureshiFaisal <qureshifi@icloud.com>
2022-10-20 14:09:13 +0530

    Hough Transform ||

--- finding_lanes/lanes.py
+++ finding_lanes/lanes.py
@@ -1,26 +1,46 @@
 import cv2
 import numpy as np
 # import matplotlib.pyplot as plt
-def canny(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    canny = cv2.Canny(blur, 50, 150)
+def canny(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    kernel = 5
+    blur = cv2.GaussianBlur(gray,(kernel, kernel),0)
+    canny = cv2.Canny(gray, 50, 150)
     return canny
 
-def region_of_interest(image):
-    height = image.shape[0]
-    polygons = np.array([[
-    (200,height),(1100,height), (550,250)
-    ]], np.int32)
+# Display the lines
+def display_lines(img,lines):
+    line_image = np.zeros_like(img)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+    return line_image
 
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
-    return mask
 
+def region_of_interest(canny):
+    height = canny.shape[0]
+    width = canny.shape[1]
+    mask = np.zeros_like(canny)
+
+    triangle = np.array([[
+    (200, height),
+    (550, 250),
+    (1100, height),]], np.int32)
+
+    cv2.fillPoly(mask, triangle, 255)
+    masked_image = cv2.bitwise_and(canny, mask)
+    return masked_image
+
+# # For detecting line edges in an image
 image = cv2.imread('test_image.jpg')
+# # Attention: Copy instead of using exact the same image
 lane_image = np.copy(image)
-canny = canny(lane_image)
-
-cv2.imshow("result", region_of_interest(canny))
+canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+# averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, lines)
+# combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', line_image)
 cv2.waitKey(0)
-//ijedqowidjq 
